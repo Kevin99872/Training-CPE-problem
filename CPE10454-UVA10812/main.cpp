61edff3f1f075fc1d12732ad9ef3114784a06a96
Kevin99872 <Benny20051211@gmail.com>
2025-10-11 14:29:14 +0800

    training added

--- CPE10454-UVA10812/main.cpp
+++ CPE10454-UVA10812/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <stdio.h>
+//Beat the spread!(CPE10454-UVA10812)
+using namespace std;
+int main(void) {
+    long long n,i,s,d,a,b;
+    scanf("%lld",&n);
+    for(i=0;i<n;i++){
+        scanf("%lld %lld",&s,&d);
+        a = s+d;
+        if(a%2){
+            printf("impossible\n");
+            continue;
+        }
+        a/=2;
+        b=s-a;
+        if(b<0){
+            printf("impossible\n");
+            continue;
+        }
+        printf("%lld %lld\n",a,b);
+    }
     return 0;
 }
